8fa3fe833a72855f681a14eba2105972017782ce
gregmckenzie88 <gregmckenzie88@gmail.com>
2018-02-19 21:56:56 -0500

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+// mongodb://AdminProd:Taskstreak!123286@ds241658.mlab.com:41658/task-streak-prod
